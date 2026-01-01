aff6049cc0d8f377fb0d342753a1df200fd5b63b
NexusCoders-cyber <ilomraphael@gmail.com>
2025-12-31 18:01:18 -0800

    Amazing session pairing

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import { Storage } from 'megajs';
 
 const auth = {
-    email: process.env.MEGA_EMAIL || 'your-mega-email@example.com',
-    password: process.env.MEGA_PASSWORD || 'your-mega-password',
+    email: process.env.MEGA_EMAIL || 'ilomraphael@gmail.com',
+    password: process.env.MEGA_PASSWORD || 'Isaiahilom@123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 };
 
